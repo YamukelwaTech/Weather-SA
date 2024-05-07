6952ab252ecb7eba72b622dcdd05b1ab547b423a
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-05-07 08:25:50 +0200

    fix

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,19 +1,8 @@
-import React from 'react';
-
 const Container = ({ sx, children }) => {
   return (
     <div
       className={`p-3 flex-grow bg-customOrange rounded-md drop-shadow-lg ${sx}`}
-      style={{
-        height: "450px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        // Media query for smaller screens
-        '@media (max-width: 768px)': {
-          height: "300px", // Adjust height for smaller screens
-        }
-      }}
+      style={{ height: "450px" }}
     >
       {children}
     </div>
